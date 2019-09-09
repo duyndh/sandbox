6f904c5c8ce6851f1553920507871900f592fd73
Duy Nguyen <ndhduy.7798@gmail.com>
2019-09-09 15:04:25 +0700

    Add logger to grpc server

--- pkg/cmd/server.go
+++ pkg/cmd/server.go
@@ -10,7 +10,9 @@ import (
 	// mysql driver
 	_ "github.com/go-sql-driver/mysql"
 
+	"github.com/ngray1747/sandbox/pkg/logger"
 	"github.com/ngray1747/sandbox/pkg/protocol/grpc"
+	"github.com/ngray1747/sandbox/pkg/protocol/rest"
 	"github.com/ngray1747/sandbox/pkg/service/v1"
 )
 
@@ -20,6 +22,8 @@ type Config struct {
 	// gRPC is TCP port to listen by gRPC server
 	GRPCPort string
 
+	// HTTP Port
+	HTTPPort string
 	// DB Datastore parameters section
 	// DatastoreDBHost is host of database
 	DatastoreDBHost string
@@ -29,6 +33,12 @@ type Config struct {
 	DatastoreDBPassword string
 	// DatastoreDBSchema is schema of database
 	DatastoreDBSchema string
+
+	// Log parameters section
+	// LogLevel is global log level: Debug(-1), Info(0), Warn(1), Error(2), DPanic(3), Panic(4), Fatal(5)
+	LogLevel int
+	// LogTimeFormat is print time format for logger e.g. 2006-01-02T15:04:05Z07:00
+	LogTimeFormat string
 }
 
 // RunServer runs gRPC server and HTTP gateway
@@ -38,14 +48,26 @@ func RunServer() error {
 	// get configuration
 	var cfg Config
 	flag.StringVar(&cfg.GRPCPort, "grpc-port", "", "gRPC port to bind")
+	flag.StringVar(&cfg.HTTPPort, "http-port", "", "HTTP port to bind")
 	flag.StringVar(&cfg.DatastoreDBHost, "db-host", "", "Database host")
 	flag.StringVar(&cfg.DatastoreDBUser, "db-user", "", "Database user")
 	flag.StringVar(&cfg.DatastoreDBPassword, "db-password", "", "Database password")
 	flag.StringVar(&cfg.DatastoreDBSchema, "db-schema", "", "Database schema")
+	flag.IntVar(&cfg.LogLevel, "log-level", 0, "Global log level")
+	flag.StringVar(&cfg.LogTimeFormat, "log-time-format", "",
+		"Print time format for logger e.g. 2006-01-02T15:04:05Z07:00")
 	flag.Parse()
 
 	if len(cfg.GRPCPort) == 0 {
 		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
+	}
+
+	if len(cfg.HTTPPort) == 0 {
+		return fmt.Errorf("Invalid HTTP Port: '%s'", cfg.HTTPPort )
+	}
+
+	if err := logger.Init(cfg.LogLevel, cfg.LogTimeFormat); err != nil {
+		return fmt.Errorf("failed to initialize logger: %v", err)
 	}
 
 	// add MySQL driver specific parameter to parse date/time
@@ -64,7 +86,12 @@ func RunServer() error {
 	}
 	defer db.Close()
 
-	v1API := v1.NewToDoServiceServer(db)
+	v1API := v1.NewTodoServiceServer(db)
+
+	// Run HTTP Server
+	go func() {
+		_ = rest.RunServer(ctx, cfg.GRPCPort, cfg.HTTPPort)
+	}()
 
 	return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
 }
--- pkg/protocol/grpc/server.go
+++ pkg/protocol/grpc/server.go
@@ -3,7 +3,6 @@ package grpc
 
 import (
 	"context"
-	"log"
 	"net"
 	"os"
 	"os/signal"
@@ -11,6 +10,8 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/ngray1747/sandbox/pkg/api/v1"
+	"github.com/ngray1747/sandbox/pkg/logger"
+	"github.com/ngray1747/sandbox/pkg/protocol/grpc/middleware"
 )
 
 func RunServer(ctx context.Context, v1API v1.TodoServiceServer, port string) error {
@@ -19,8 +20,14 @@ func RunServer(ctx context.Context, v1API v1.TodoServiceServer, port string) err
 		return error
 	}
 
+	// gRPC server statup options
+	opts := []grpc.ServerOption{}
+
+	// add middleware
+	opts = middleware.AddLogging(logger.Log, opts)
+
 	// Register GRPC server
-	server := grpc.NewServer()
+	server := grpc.NewServer(opts...)
 	v1.RegisterTodoServiceServer(server, v1API)
 
 	// Singal to shutdown
@@ -28,12 +35,12 @@ func RunServer(ctx context.Context, v1API v1.TodoServiceServer, port string) err
 	signal.Notify(cancel, os.Interrupt)
 	go func() {
 		for range cancel {
-			log.Println("Shutting down GRPC server")
+			logger.Log.Warn("Shutting down GRPC server")
 			server.GracefulStop()
 			<- ctx.Done()
 		}
 	}()
 
-	log.Println("Starting GRPC server")
+	logger.Log.Info("Starting GRPC server")
 	return server.Serve(listen)
 }
